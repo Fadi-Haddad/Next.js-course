ceea9566edb34bd09433c982b3a55e3ce08984da
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-13 21:27:12 +0300

    Add a directory to be ignored in routing

---
+++ app/(outer)/inner/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const inner = () => {
+  return (
+    <div>inner text</div>
+  )
+}
+
+export default inner
+
+// when ignoring a directoy in routing, we don't have to create a page.js file in the ignored directory
